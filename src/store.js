0f524f0991532eebb265e28d45ec9cc279335e98
Rohit paul <45958413+Rohitpaul98@users.noreply.github.com>
2021-11-10 11:44:31 +0530

    noob

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 import { createStore } from "@reduxjs/toolkit";
-
 import rootReducer from "./reducers";
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
